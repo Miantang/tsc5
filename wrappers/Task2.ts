4c96e3e7f43f4847daf81df483e94fe22fffbb5b
Miantang <miantang.cn@hotmail.com>
2023-12-25 00:28:11 +0800

    test

--- wrappers/Task2.ts
+++ wrappers/Task2.ts
@@ -1,12 +1,13 @@
 import { Address, beginCell, Dictionary, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from 'ton-core';
 import { randomAddress } from '@ton-community/test-utils';
 
-export type Task2Config = {};
+export type Task2Config = {
+    admin: Address,
+};
 
 export function task2ConfigToCell(config: Task2Config): Cell {
-    const address = randomAddress();
     return beginCell()//.endCell();
-    .storeAddress(address)
+    .storeAddress(config.admin)
     .storeDict(Dictionary.empty())
     .endCell();
 }
@@ -31,23 +32,71 @@ export class Task2 implements Contract {
             body: beginCell().endCell(),
         });
     }
-
-    async sendDict(provider: ContractProvider, via: Sender, value: bigint) {
+    async sendSplit(provider: ContractProvider, via: Sender, value: bigint) {
         await provider.internal(via, {
                 value,
                 sendMode: SendMode.PAY_GAS_SEPARATELY,
                 body: beginCell()
-                .storeUint(0x66666666, 32)
-                .storeAddress(via.address)
+                .storeUint(0x068530b3, 32)
+                .storeUint(0x1414, 64)
                 .endCell()
             })
     }
 
+    async sendAddUser(provider: ContractProvider, via: Sender, value: bigint, addr: Address, share: number = 99) {
+        await provider.internal(via, {
+            value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell()
+            .storeUint(0x368ddef3, 32)
+            .storeUint(0x1415, 64) // query_id
+            .storeAddress(addr)
+            .storeUint(share, 32)
+            .endCell()
+        })
+    }
+
+    async sendDelUser(provider: ContractProvider, via: Sender, value: bigint, addr: Address) {
+        await provider.internal(via, {
+            value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell()
+            .storeUint(0x278205c8, 32)
+            .storeUint(0x1416, 64) // query_id
+            .storeAddress(addr)
+            .endCell()
+        })
+    }
+
+    async sendDict(provider: ContractProvider, via: Sender, value: bigint) {
+        await provider.internal(via, {
+            value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell()
+            .storeUint(0x66666666, 32)
+            .storeAddress(via.address)
+            .endCell()
+        })
+    }
+
     async getState(provider: ContractProvider) {
-        const {stack} = await provider.get('get_users', []);
-        return {
-            cell: stack.readCell()
-        }
+        const {stack} = await provider.get('get_admin', []);
+        return stack.readAddress()
         // return await provider.getState()
+    }
+
+    async getUsers(provider: ContractProvider) {
+        const {stack} = await provider.get('get_users', []);
+        return stack.readCellOpt()?.asSlice().loadDictDirect({
+            bits: 256, parse: (src) => src, serialize: (src) => src
+        }, {
+            serialize: (src, builder) => builder.storeUint(src as number, 32), 
+            parse: (slice) => slice.loadUint(32),
+        })
+    }
+
+    async getUserShare(provider: ContractProvider, addr: Address) {
+        const {stack} = await provider.get('get_user_share', [{type: 'slice', cell: beginCell().storeAddress(addr).endCell()}]);
+        return stack.readNumberOpt();
     }
 }
