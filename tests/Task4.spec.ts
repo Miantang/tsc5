1e2a96375989707c7918f68e7bb880eda9120ea2
Miantang <miantang.cn@hotmail.com>
2023-12-26 22:40:22 +0800

    try task4

--- tests/Task4.spec.ts
+++ tests/Task4.spec.ts
@@ -4,6 +4,60 @@ import { Task4 } from '../wrappers/Task4';
 import '@ton-community/test-utils';
 import { compile } from '@ton-community/blueprint';
 
+const maze1 = [
+    'XXXXXXE.',
+    'XX.XXXX.',
+    'X.X.XXXX',
+    '.?XSXXX.',
+    '?.XXXXX.',
+    'XX..XXX.',
+    'XX..XX?X',
+    'XXX...XX'
+  ];
+
+const maze0 = [
+    'XXXXXXE.',
+    'XX.XXXX.',
+    'X.X.XXX.',
+    '.?XSXXX.',
+    '?.XXXXX.',
+    'XX..XXX.',
+    'XX..XX?X',
+    'XXX...XX'
+  ];
+const maze2 = [
+    'SX.?X',
+    '.XX.X',
+    'X.?..',
+    '.??..',
+    'X?...',
+    '..X.X',
+    '..?..',
+    'X...E'
+  ]
+
+  const maze4 = [
+    'SX.?XXXX',
+    '.XX.XXXX',
+    'X.?..XXX',
+    '.??..XXX',
+    'X?...XXX',
+    '..X.XXXX',
+    '..?..XXX',
+    'X...EXXX'
+  ]
+
+  const maze5 = [
+    'SX.?XXXX',
+    '.XX.XXXX',
+    'X.?..XXX',
+    '.??..XXX',
+    'X?...XXX',
+    '..X.XXXX',
+    '..X?XXXX',
+    'XX..EXXX'
+  ]
+
 describe('Task4', () => {
     let code: Cell;
 
@@ -34,5 +88,11 @@ describe('Task4', () => {
     it('should deploy', async () => {
         // the check is done inside beforeEach
         // blockchain and task4 are ready to use
+    });
+
+    it('should solve', async () => {
+        // const res = await task4.sendSolve(8n, 8n, maze5);
+        // console.log('res', res.result);
+        // const res2 = await task4Basic.sendSolve(5n, 8n, maze2)
     });
 });
--- wrappers/Task4.ts
+++ wrappers/Task4.ts
@@ -1,9 +1,35 @@
-import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from 'ton-core';
+import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode, Tuple, TupleReader, TupleItemInt  } from 'ton-core';
 
 export type Task4Config = {};
 
 export function task4ConfigToCell(config: Task4Config): Cell {
     return beginCell().endCell();
+}
+
+
+const parseStringToTuple = (line: string) => line.split('').map((c: string) => ({type: 'int', value: BigInt(c.charCodeAt(0))}) as TupleItemInt);
+const parseStringArray = (maze: string[]) => {
+    return maze.map((line) => {
+        return { type: 'tuple', items: parseStringToTuple(line) } as Tuple
+    })
+}
+
+function parseTupleArrayAuto(tr: TupleReader | null, withString: boolean = true) {
+    if(!tr) {
+        return null;
+    }
+    return new Array(Number(tr.remaining)).fill(0).map((_: any, index: number) => {
+        const lineReader = tr.readTuple();
+        if(withString) {
+            return new Array(Number(lineReader.remaining)).fill(0).map((_: any, i2: number) => {
+                return String.fromCharCode(lineReader.readNumber());
+            }).join('');
+        }
+        return new Array(Number(lineReader.remaining)).fill(0).map((_: any, i2: number) => {
+            return lineReader.readNumber();
+        });
+        
+    })
 }
 
 export class Task4 implements Contract {
@@ -25,5 +51,36 @@ export class Task4 implements Contract {
             sendMode: SendMode.PAY_GAS_SEPARATELY,
             body: beginCell().endCell(),
         });
+    }
+
+    async sendSolve(provider: ContractProvider, n: bigint, m: bigint, maze: string[]) {
+        const nn: bigint = BigInt(maze.length);
+        const mm: bigint = BigInt(maze[0].length);
+        const {stack} = await provider.get('solve', [
+            {type: 'int', value: nn},
+            {type: 'int', value: mm},
+            {type: 'tuple', items: parseStringArray(maze)},
+        ]);
+
+        // const resItems =  await stack.readTuple();
+        // console.log('stack', stack);
+        return [stack.readNumberOpt(), stack.readNumberOpt(), stack.readNumberOpt(),  parseTupleArrayAuto(stack.readTupleOpt())];
+        // const arranged = resItems.map((t: TupleItem, i: number) => {
+        //     if(t.type === 'int') {
+        //         return parseTupleItemInt(t);
+        //     }
+        //     if(t.type === 'null') {
+        //         return t;
+        //     }
+        //     if(t.type === 'tuple') {
+        //         return t.items.map((t: TupleItem) => {
+        //             if(t.type === 'int') {
+        //                 return parseTupleItemInt(t);
+        //             }
+        //             return t;
+        //         })
+        //     }
+        // })
+        // return arranged;
     }
 }
