55efa7c5c46e656b3a5f094c22aa6089e4f03721
Miantang <miantang.cn@hotmail.com>
2023-12-27 19:11:43 +0800

    track

--- scripts/deployTask2.ts
+++ scripts/deployTask2.ts
@@ -1,9 +1,13 @@
 import { toNano } from 'ton-core';
 import { Task2 } from '../wrappers/Task2';
 import { compile, NetworkProvider } from '@ton-community/blueprint';
+import { randomAddress } from '@ton-community/test-utils';
 
 export async function run(provider: NetworkProvider) {
-    const task2 = provider.open(Task2.createFromConfig({}, await compile('Task2')));
+    const sender = provider.sender();
+    const task2 = provider.open(Task2.createFromConfig({
+        admin: sender.address || randomAddress()
+    }, await compile('Task2')));
 
     await task2.sendDeploy(provider.sender(), toNano('0.05'));
 
