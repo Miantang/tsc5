ba135348e19dfe44586ee6f95a4f4e787bd6291d
Miantang <miantang.cn@hotmail.com>
2024-01-05 14:21:26 +0800

    load_ref

--- tests/Task3.spec.ts
+++ tests/Task3.spec.ts
@@ -35,19 +35,19 @@ describe('Task3', () => {
         // the check is done inside beforeEach
         // blockchain and task3 are ready to use
     });
-    it('should v1', async () => {
-        // the check is done inside beforeEach
-        // blockchain and task3 are ready to use
-        const deployer = await blockchain.treasury('deployer');
-        const sender = deployer.getSender();
-        const m0 = await task3.sendFirst(sender);
-        const m = await task3.sendV1(sender);
-        const version = await task3.getVersion();
-        // console.log('m', m.transactions, version);
-        expect(m.transactions).toHaveTransaction({
-            success: true,
-        });
-    });
+    // it('should v1', async () => {
+    //     // the check is done inside beforeEach
+    //     // blockchain and task3 are ready to use
+    //     const deployer = await blockchain.treasury('deployer');
+    //     const sender = deployer.getSender();
+    //     const m0 = await task3.sendFirst(sender);
+    //     const m = await task3.sendV1(sender);
+    //     const version = await task3.getVersion();
+    //     // console.log('m', m.transactions, version);
+    //     expect(m.transactions).toHaveTransaction({
+    //         success: true,
+    //     });
+    // });
 
     it('should v2', async () => {
         // the check is done inside beforeEach
--- wrappers/Task3.ts
+++ wrappers/Task3.ts
@@ -1,8 +1,8 @@
 import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Dictionary, Sender, SendMode , toNano} from 'ton-core';
 
-const c1: Cell = Cell.fromBase64('te6ccgEBCgEAqAABFP8A9KQT9LzyyAsBAgFiAgMCAs4EBQARoFTv2omhrhY/AgEgBgcCASAICQCtCDXScEgkl8E4NMf9AT0BDHUMCLAAI4iMiBus5gg0O0e7VP7BJEw4u1E0NQwAdAUQzDwAXEB8ALtVOAx7UTQ0x8wUSK58tGQ8AMB0BQQNUFQ8AHwAu1UgABcXwTQ1wsfpMjLH8mAADQByMsfzMmAAEztRNCAINch1DCA=');
-const c2: Cell = Cell.fromBase64('te6ccgECDwEAATAAART/APSkE/S88sgLAQIBYgIDAgLOBAUCASALDAIBIAYHAgEgCQoByQg10nBIJJfBODTH/QE9ATUJMAAjiQwMTIgbrOYINDtHu1T+wSRMOLtRNDUMAHQFEMw8AFxAfAC7VTgM+1E0NMfMFNAufLwU0C84wJsIlEiup/wAwHQFBA1QVDwAfAC7VSSXwXigCAAhGwxAdDXCx8B0x8woMjLH8mAAogP0BDAgbvLQyCDQ7R7tU/sEWYAg9A5voTDTH/QEMCBus5gg0O0e7VP7BJEw4lIDup/wAwHQFBA1QVDwAfAC7VTgMfADAdAUQzDwAXMB8ALtVAANAHIyx/MyYAATO1E0IAg1yHUMIAARvVO/aiaGuFj8AgEgDQ4AEbjffwA9DXCx+AATu0D4IBjfftQ9iA==');
-const c3: Cell = Cell.fromBase64('te6ccgEBDgEAjwABFP8A9KQT9LzyyAsBAgFiAgMCAs4EBQIBIAoLAgEgBgcCASAICQA9IAg1yH0BDH0BDHUMO1E0NQwAdAUQzDwAX4B8ALtVIAAhGwxAdDXCycB0ycwoMjLJ8mAACQxyMzJgAAs7UTQ1DCAAB71Tu/QCASAMDQARuN9/AD0NcLJ4ABu7QPggGN9+1D2IBkqQSA==');
+const c1: Cell = Cell.fromBase64('te6ccgECDQEAATMAART/APSkE/S88sgLAQIBYgIDAgLNBAUAEaBU79qJoa4WPwIBIAYHABPXaiaEAQa5DqGEAgEgCAkCASALDAHJCDXScEgkl8E4NMf9AT0BNQwI8AAjiMxMiBus5gg0O0e7VP7BJEw4u1E0NQwAdAUQzDwAXEB8ALtVODtRNDTH1NRufLRkFNRvOMCMGwiUSK6n/AEAdAUEDVBUPAB8ALtVJJfBeKAKABcXwTQ1wsfpMjLH8mAA5CRu8tDIJNDtHu1TBPsEIoAg9A5vofLhkCDHAPLRkCDHAvLRkNMf9AQwBNQwjjQkbrOYJNDtHu1T8APeUxOAIPQOb6EhxwDy0ZAhxwLy0ZCXMjTTH/QEMJUw8sDIBOJTFboV5jEzMdAUEDVBUPAB8ALtVAANAHIyx/MyYAABIA==');
+const c2: Cell = Cell.fromBase64('te6ccgECDwEAAVMAART/APSkE/S88sgLAQIBYgIDAgLOBAUCASALDAIBIAYHAgEgCQoByQg10nBIJJfBODTH/QE9ATUMCPAAI4jMTIgbrOYINDtHu1T+wSRMOLtRNDUMAHQFEMw8AFxAfAC7VTg7UTQ0x9TUbny0ZBTUbzjAjBsIlEiup/wAwHQFBA1QVDwAfAC7VSSXwXigCAAhGwxAdDXCx8B0x8woMjLH8mAA6CRu8tDIJNDtHu1TBPsEIoAg9A5vofLhkCDHAPLRkCDHAvLRkNMf9AQwBNQwjjYkbrOaJNDtHu1T+EHaEd5TE4Ag9A5voSHHAPLRkCHHAvLRkJcyNNMf9AQwlTDywMgE4lMVuhXmMTMx0BQQNUFQ8AHwAu1UAA0AcjLH8zJgABM7UTQgCDXIdQwgABG9U79qJoa4WPwCASANDgARuN9/AD0NcLH4ABO7QPggGN9+1D2I');
+const c3: Cell = Cell.fromBase64('te6ccgECEQEAAU4AART/APSkE/S88sgLAQIBYgIDAgLNBAUCASANDgIBIAYHABPXaiaEAQa5DqGEAgEgCAkCASALDAHJCDXScEgkl8E4NMf9AT0BNQwI8AAjiMxMiBus5gg0O0e7VP7BJEw4u1E0NQwAdAUQzDwAXEB8ALtVODtRNDTH1NRufLRkFNRvOMCMGwiUSK6n/AEAdAUEDVBUPAB8ALtVJJfBeKAKACEbDEB0NcLJwHTJzCgyMsnyYACyJG7y0Mgk0O0e7VME+wQigCD0Dm+hMNMf9AQwBNQwk1MVuo4lJG6zmCTQ7R7tU/AD3lMTgCD0Dm+hlzI00x/0BDCVMPLAyATiBOgxMzHQFBA1QVDwAfAC7VQADQByMsfzMmAAFTQ1wsfp2TIyyfJgABG9U79qJoa4WPwCASAPEAARuN9/AE0NcLJ4ABu7QPggGN9+1D2IBkqQSA==');
 
 export type Task3Config = {};
 
@@ -59,25 +59,35 @@ export class Task3 implements Contract {
             .storeUint(1, 32)
             .storeMaybeRef(c1)
             .storeDict(Dictionary.empty())
-            .storeMaybeRef(beginCell().endCell())
+            .storeRef(beginCell().endCell())
             .endCell(),
         });
     }
 
     async sendV2(provider: ContractProvider, via: Sender) {
+        const dic = Dictionary.empty({
+            bits: 32, parse: (src) => src, serialize: (src) => src
+        },{
+            serialize: (src: any, builder) => builder.storeSlice(src.beginParse()), 
+            parse: (slice) => ({
+                new_version: slice.loadUint(32),
+                mig: slice.loadMaybeRef(),
+            }),
+        }).set(2n, beginCell().storeUint(2, 32).storeMaybeRef().endCell()).set(1n, beginCell().storeUint(2, 32).storeMaybeRef(c2).endCell());
+        // console.log('dic', dic.get(2n).asSlice().loadDictDirect({
+        //     bits: 32, parse: (src: bigint) => src, serialize: (src: bigint) => src
+        // }, {
+        //     serialize: (src: any, builder: any) => builder.storeUint(src as number, 32), 
+        //     parse: (slice: any) => slice.loadUint(32),
+        // }));
         await provider.internal(via, {
             value: toNano(0.1),
             sendMode: SendMode.PAY_GAS_SEPARATELY,
             body: beginCell()
             .storeUint(2, 32)
             .storeMaybeRef(c2)
-            .storeDict(Dictionary.empty({
-                bits: 32, parse: (src) => src, serialize: (src) => src
-            },{
-                serialize: (src: any, builder) => builder.storeMaybeRef(src), 
-                parse: (slice) => slice.loadMaybeRef(),
-            }).set(2n, beginCell().endCell()).set(1n, beginCell().storeUint(2, 32).storeMaybeRef(c2).endCell()))
-            .storeMaybeRef(beginCell().endCell())
+            .storeDict(dic)
+            .storeRef(beginCell().endCell())
             .endCell(),
         });
     }
@@ -88,10 +98,15 @@ export class Task3 implements Contract {
             body: beginCell()
             .storeUint(3, 32)
             .storeMaybeRef(c3)
-            .storeDict(Dictionary.empty()
-                .set(1n, beginCell().endCell())
-                .set(2n, beginCell().storeMaybeRef(c2).endCell())
-                .set(3n, beginCell().storeMaybeRef(c3).endCell())
+            .storeDict(Dictionary.empty({
+                bits: 32, parse: (src) => src, serialize: (src) => src
+            },{
+                serialize: (src: any, builder) => builder.storeMaybeRef(src), 
+                parse: (slice) => slice.loadMaybeRef(),
+            })
+                .set(1n, beginCell().storeUint(2, 32).storeMaybeRef().endCell())
+                .set(2n, beginCell().storeUint(3, 32).storeMaybeRef(c3).endCell())
+                .set(3n, beginCell().storeUint(3, 32).storeMaybeRef().endCell())
             )
             .storeMaybeRef(beginCell().endCell())
             .endCell(),
